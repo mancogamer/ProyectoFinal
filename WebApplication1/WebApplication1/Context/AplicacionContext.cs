f4f55530856baacc747aa42b08023cc3c766b462
ronaldCervantes <ronaldcervantes31@gmail.com>
2023-11-20 23:38:14 -0400

    subiendo controlador y modelo de InformacionEscolar

---
+++ WebApplication1/WebApplication1/Context/AplicacionContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Controllers;
+using WebApplication1.Models;
+
+namespace WebApplication1.Context
+{
+    public class AplicacionContext : DbContext
+    {
+        public AplicacionContext (DbContextOptions<AplicacionContext> options)
+            : base(options) 
+        {
+        
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Asignatura> Asignaturas { get; set; }
+        public DbSet<recursos_educativo> RecursosEducativos { get; set; }
+        public DbSet<AnuncioInformacionEscolar> AnunciosInformacionEscolar { get; set; }
+    }
+}
