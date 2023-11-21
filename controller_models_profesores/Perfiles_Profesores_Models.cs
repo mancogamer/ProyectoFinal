fbe1ad91a0fbc86f1f34ec9c6cb7655f4c69d92f
mancogamer <147411671+mancogamer@users.noreply.github.com>
2023-11-21 00:50:51 -0400

    subiendo los modelos de perfiles maestros

---
+++ controller_models_profesores/Perfiles_Profesores_Models.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAplication.Models
+{
+    public class perfiles_profesores
+    {
+        [Key]
+
+        public int id { get; set; }
+        
+        public int usuario { get; set; }
+
+        public string informacion_personal { get; set; }
+
+        public string informacion_academica { get; set; }
+
+        public string informacion_contacto { get; set; }
+
+        public string foto { get; set; }
+    }
+}
